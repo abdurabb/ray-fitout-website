01f2969ffe359f83aba496f98e3fec6d47c76289
AbdulRubb <abdulrubb@AbdulRubbs-MacBook-Air.local>
2025-09-04 10:48:19 +0400

    text changed in banner

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -10,15 +10,15 @@ function Banner() {
         >
             {/* Text content */}
             <div className="relative z-10 text-white w-full px-8 md:px-12 py-8 md:py-12 lg:py-16 mx-auto">
-                <p className="uppercase text-xs sm:text-sm md:text-sm font-extralight opacity-0 fade-up delay-1">
+                <p className="uppercase text-sm sm:text-sm md:text-lg font-extralight opacity-0 fade-up delay-1">
                     Services / Aluminium Systems
                 </p>
 
-                <h1 className="uppercase font-light text-lg sm:text-2xl md:text-4xl lg:text-5xl mt-2 opacity-0 fade-up delay-2">
+                <h1 className="uppercase font-light text-lg sm:text-3xl md:text-4xl lg:text-5xl mt-2 opacity-0 fade-up delay-2">
                     Aluminum Systems
                 </h1>
 
-                <p className="uppercase text-xs sm:text-sm md:text-sm font-extralight mt-2 opacity-0 fade-up delay-3">
+                <p className="uppercase text-sm sm:text-sm md:text-lg font-extralight mt-2 opacity-0 fade-up delay-3">
                     Contemporary Systems, Seamless Integration
                 </p>
             </div>
