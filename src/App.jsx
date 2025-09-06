18a03f8438b18b4532a00c9c5f23da87bf1f16c4
AbdulRubb <abdulrubb@AbdulRubbs-MacBook-Air.local>
2025-09-06 17:50:12 +0400

    * route

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         <Routes>
           <Route element={<Main />}>
             <Route path="/" element={<Home />} />
+            <Route path="*" element={<Home />} />
           </Route>
         </Routes>
       </BrowserRouter>
